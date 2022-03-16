3366f4f0827c24cb07af7bbd1d603eba0e040e86
charlieohara <34257389+charlieohara@users.noreply.github.com>
2022-03-16 10:43:01 -0400

    packages to use

---
+++ text_to_phones.py
@@ -0,0 +1,24 @@
+import urllib.request
+with open("cmudict","r") as cmu:
+    cmu_txt=cmu.readlines()
+pronunciations={}
+for line in cmu_txt:
+    pronunciations[line.split()[0]]=line.split()[2:]
+
+def text_to_phones(text):
+    sentence=[]
+
+    for word in text.split():
+        word=word.upper()
+        if word in pronunciations:
+            sentence.append(pronunciations[word])
+        else:
+            print(word," is not in CMU.")
+            sentence.append([])
+    return sentence
+
+def input_to_phones():
+    return text_to_phones(input("What text do you want to be synthesized?"))
+
+print(text_to_phones("let's give this a good old tryy!"))
+input_to_phones()
